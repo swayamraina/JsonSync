d6b6ef121c0d13afbb91f2b39361956fcc780899
Swayam Raina <code.monk.wi@gmail.com>
2017-08-02 19:36:29 +0530

    Added class to contain mismatch data

---
+++ src/main/com/swayamraina/JsonSync/JsonElement.java
@@ -0,0 +1,30 @@
+package main.com.swayamraina.JsonSync;
+
+public class JsonElement {
+	
+	private String element;
+	private int type;
+	
+	public String getElement() {
+		return element;
+	}
+
+	public void setElement(String element) {
+		this.element = element;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+	public JsonElement(String element, int type) {
+		this.setElement(element);
+		this.setType(type);
+	}
+}
+
+
