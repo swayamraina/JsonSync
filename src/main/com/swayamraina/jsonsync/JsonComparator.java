231a3261795dcee38af9db135d97cd3fc0dce99b
Swayam Raina <code.monk.wi@gmail.com>
2017-08-19 12:05:37 +0530

    Added key comparison method

--- src/main/com/swayamraina/jsonsync/JsonComparator.java
+++ src/main/com/swayamraina/jsonsync/JsonComparator.java
@@ -1,8 +1,38 @@
 package main.com.swayamraina.jsonsync;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class JsonComparator {
 	
+	private static final int INSERT = 1;
+	private static final int DELETE = 2;
 	
-	
+	/**
+	 * This method compares keys of a particular level and collects
+	 * keys that are needed to be updated.
+	 * 
+	 * The updates collected are to be done on data files
+	 * 
+	 * @param oldTemplate
+	 * @param newTemplate
+	 * @return
+	 */
+	private static List<JsonElement> compareLevelObjectHelper(Set<String> oldTemplate, Set<String> newTemplate) {
+		List<JsonElement> diffElements = new ArrayList<>();
+		for(String oldTemplateKey : oldTemplate) {
+			if(!newTemplate.contains(oldTemplateKey)) {
+				diffElements.add(new JsonElement(oldTemplateKey, DELETE));
+			}
+		}
+		for(String newTemplateKey : newTemplate) {
+			if(!oldTemplate.contains(newTemplateKey)) {
+				diffElements.add(new JsonElement(newTemplateKey, INSERT));
+			}
+		}
+		return diffElements;
+	}
+
 }
 
