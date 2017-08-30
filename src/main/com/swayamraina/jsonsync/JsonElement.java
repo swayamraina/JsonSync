86d4a4cf5101720ee5f257ef41cb85e8d7db1904
Swayam Raina <code.monk.wi@gmail.com>
2017-08-30 21:34:16 +0530

    Updated variable names

--- src/main/com/swayamraina/jsonsync/JsonElement.java
+++ src/main/com/swayamraina/jsonsync/JsonElement.java
@@ -2,28 +2,28 @@ package main.com.swayamraina.jsonsync;
 
 public class JsonElement {
 	
-	private String element;
-	private int type;
+	private String elementPath;
+	private int action;
+
+	public String getElementPath() {
+		return elementPath;
+	}
+
+	public void setElementPath(String elementPath) {
+		this.elementPath = elementPath;
+	}
+
+	public int getAction() {
+		return action;
+	}
+
+	public void setAction(int action) {
+		this.action = action;
+	}
 	
-	public String getElement() {
-		return element;
-	}
-
-	public void setElement(String element) {
-		this.element = element;
-	}
-
-	public int getType() {
-		return type;
-	}
-
-	public void setType(int type) {
-		this.type = type;
-	}
-	
-	public JsonElement(String element, int type) {
-		this.setElement(element);
-		this.setType(type);
+	public JsonElement(String elementPath, int action) {
+		this.setElementPath(elementPath);
+		this.setAction(action);
 	}
 	
 	@Override
@@ -31,7 +31,7 @@ public class JsonElement {
 	    if(other == this) return true;
 	    if(!(other instanceof JsonElement)) return false;
 	    JsonElement tempJsonElement = (JsonElement) other;
-	    return (tempJsonElement.getElement().equals(this.getElement()) && tempJsonElement.getType()==this.getType()) ? true : false;
+	    return (tempJsonElement.getElementPath().equals(this.getElementPath()) && tempJsonElement.getAction()==this.getAction()) ? true : false;
 	}
 }
 
